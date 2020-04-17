7367ba4a3c6439f82d4ac2a2443d853721067a1d
RodrigoAk <rodrigo.h.akamine@usp.br>
2020-04-17 17:19:27 -0300

    Renamed files and added one exercise

---
+++ problems/ex6.py
@@ -0,0 +1,18 @@
+'''
+Given a list of numbers with only 3 unique numbers
+(1, 2, 3), sort the list in O(n) time.
+
+Esse é o 'Dutch national flag problem'
+
+Example:
+Input: [3, 3, 2, 1, 3, 2, 1]
+Output: [1, 1, 2, 2, 3, 3, 3]
+'''
+
+
+def sortNums(nums):
+    # Fill this in
+
+
+print(sortNums([3, 3, 2, 1, 3, 2, 1]))
+# [1, 1, 2, 2, 3, 3, 3]
